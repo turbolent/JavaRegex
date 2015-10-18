e2db566f04bf6567f4c1b159f7c92d7651bcadd9
Bastian Müller <bastian@turbolent.com>
2015-10-18 15:16:40 +0200

    CI: deploy to maven repository

---
+++ .travis/deploy.sh
@@ -0,0 +1,22 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  export PROJECT_NAME=$(basename $TRAVIS_REPO_SLUG)
+  echo -e "Deploying '$PROJECT_NAME' to maven repo:\n"
+
+  git config --global user.email "travis-ci@users.noreply.github.com"
+  git config --global user.name "travis-ci"
+  git config --global push.default simple
+
+  echo -e "Cloning maven repo ...\n"
+  export REPO_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t mvn-repo)
+  git clone https://$GH_TOKEN:x-oauth-basic@github.com/turbolent/mvn-repo.git $REPO_DIR
+
+  echo -e "Running maven deploy ...\n"
+  mvn deploy -DrepositoryPath=$REPO_DIR
+
+  echo -e "Committing and pushing ...\n"
+  cd $REPO_DIR
+  git commit -a -m "update $PROJECT_NAME"
+  git push
+
+  echo -e "Deployed.\n"
+fi
